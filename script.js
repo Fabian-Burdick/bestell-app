935ac0417028a509de35a4d29f21a73e14175fec
Fabian-Burdick <Fabian-Burdick@web.de>
2026-07-27 20:52:31 +0200

    Add: Basket Templates without function

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function allDishes() {
   renderDishes('burgerContainer', 'burger');
   renderDishes('pizzaContainer', 'pizza');
   renderDishes('saladContainer', 'salad');
+  renderEmptyBasket();
 }
 
 function renderDishes(containerId, category) {
@@ -14,5 +15,22 @@ function renderDishes(containerId, category) {
   dishMenu.innerHTML = '';
   for (let i = 0; i < dishes.length; i++) {
     dishMenu.innerHTML += dishesTemplate(dishes[i]);
+  }
+}
+
+// function pushDishesToBasket(containerId, category) {
+//   const dishMenu = document.getElementById(containerId);
+//   const dishes = myDishes.find((dish) => dish.category === category);
+//   dishMenu.innerHTML = '';
+//   for (let i = 0; i < dishes.length; i++) {
+//     dishMenu.innerHTML += dishesTemplate(dishes[i]);
+//   }
+// }
+
+function renderEmptyBasket() {
+  const basketContainer = document.getElementById('basketId');
+
+  if (basketContainer) {
+    basketContainer.innerHTML = emptyBasketTemplate();
   }
 }
--- scripts/tamplate.js
+++ scripts/tamplate.js
@@ -10,10 +10,61 @@ function dishesTemplate(dishes) {
                             </div>
                             <div class="menu-price">
                                 <h6>${dishes.price.toFixed(2).replace('.', ',')} €</h6>
-                                <button onclick="menuButtons()">
-                                    <p id="menuButton">Add to basket </p>
+                                <button onclick="pushDishesToBasket()">
+                                    Add to basket 
                                 </button>
                             </div>
                         </div>
                     </div>`;
+}
+
+function BasketDishesTemplate() {
+  return `<div class="basket">
+      <div class="basket-content">
+        <h4>Your Basket</h4>
+        <div class="basket-menucard">
+          <div>
+            <p>${dishes.name}</p>
+          </div>
+          <div class="basket-menucard-container">
+            <div class="basket-menucard-text">
+              <img src="./assets/icons/bin.webp" alt="bin" />
+              <p>1+</p>
+            </div>
+            <p>${dishes.price.toFixed(2).replace('.', ',')} €</p>
+          </div>
+        </div>
+        <div class="total-content">
+          <div class="total-price">
+            <p>Subtotal</p>
+            <p>Price</p>
+          </div>
+          <div class="total-price">
+            <p>Dilivery fee</p>
+            <p>Price</p>
+          </div>
+          <div class="underline"></div>
+          <div class="total">
+            <p>Total</p>
+            <p>Price</p>
+          </div>
+          <div>
+            <button>Buy</button>
+          </div>
+        </div>
+      </div>
+    </div>`;
+}
+
+function emptyBasketTemplate() {
+  return `<div class="basket">
+            <div class="basket-content">
+              <h4>Your Basket</h4>
+              <p>
+                Nothing here yet.<br />
+                Go ahead and choose something delicious!
+              </p>
+              <img src="./assets/icons/basket.webp" alt="Basket" />
+            </div>
+        </div>`;
 }
